97513ec572c794db0e7ed49efb6899cf4ff7c262
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-03-05 14:59:47 +0100

    Modify App's paragraph

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my first (kind of) React app! Yay!
         </p>
         <a
           className="App-link"
